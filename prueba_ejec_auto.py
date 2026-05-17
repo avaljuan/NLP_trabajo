decc2a37eb16092f02ad750c1900e6cd54a2071c
avaljuan <avaljuan@alumni.uv.es>
2026-05-17 22:26:32 +0200

    Add files via upload

---
+++ prueba_ejec_auto.py
@@ -0,0 +1,9 @@
+import datetime
+
+print("=======================================")
+print("🚀 ¡ÉXITO! GitHub Actions está funcionando.")
+print("=======================================")
+
+hora_actual = datetime.datetime.now()
+print(f"El script se ha ejecutado el: {hora_actual}")
+print("¡Todo listo para subir tu orquestador de noticias!")
