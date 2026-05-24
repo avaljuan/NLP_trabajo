5e460f26f92fa8bf083b3ba24c42540424c3cbc1
jomercre <jomercre@alumni.uv.es>
2026-05-24 17:29:38 +0200

    Eliminacion archivo redundante

--- prueba_ejec_auto.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-print("=======================================")
-print("🚀 ¡ÉXITO! GitHub Actions está funcionando.")
-print("=======================================")
-
-hora_actual = datetime.datetime.now()
-print(f"El script se ha ejecutado el: {hora_actual}")
-print("¡Todo listo para subir tu orquestador de noticias!")
